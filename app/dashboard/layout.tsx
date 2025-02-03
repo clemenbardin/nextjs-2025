31e11271c8c299d8d146184df27bd68c197320a1
cbardin <bardinclement97@gmail.com>
2025-02-03 15:04:01 +0100

    Chapitre 10

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,21 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
+// app/layout.tsx (ou un nouveau fichier app/components/layout.tsx)
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/dashboard/sidenav'; // Importez votre SideNav
 
-export default function Layout({ children }: { children: React.ReactNode}) {
-    return (
-        <div className="flex h-screen flex-col md-flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
-                <SideNav />
-            </div>
-            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{ children }</div>
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr">
+      <body className={`${inter.className} antialiased`}>
+        <div className="flex min-h-screen">
+          <aside className="w-64 md:w-80">
+            <SideNav />
+          </aside>
+          <main className="mt-3 ml-5 overflow-y-auto"> {/* Contenu principal (Dashboard) */}
+            {children}
+          </main>
         </div>
-    )
+      </body>
+    </html>
+  );
 }
